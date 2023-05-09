6e20dd193c79733d3b04d23ed3f7b84a07bca869
akankshamakkar <akimakkar1282@gmai.com>
2023-05-09 22:23:01 +0530

    all implementations

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,4 +1,13 @@
 public interface BankAccount {
     // withdraw money
+    // check balance
+    // add money
+    // get ROI
+    // get totalInterest
     Boolean withdrawMoney(int money);
+    Boolean addMoney(int money);
+    int checkBalance();
+    int getROI();
+    int getTotalInterest(int amount, int timeInYears);
+
 }
--- src/HDFCBankAccount.java
+++ src/HDFCBankAccount.java
@@ -1,8 +1,20 @@
+import java.util.UUID;
+
 public class HDFCBankAccount implements BankAccount{
+    private String accountNumber;
     private int balance;
+    private int roi; // 5% - 5
 
     public HDFCBankAccount(int balance) {
         this.balance = balance;
+        this.accountNumber = String.valueOf(UUID.randomUUID());
+        this.roi = 5;
+    }
+
+    public HDFCBankAccount(int balance, int roi) {
+        this.balance = balance;
+        this.accountNumber = String.valueOf(UUID.randomUUID());
+        this.roi = roi;
     }
 
     public int getBalance() {
@@ -13,6 +25,18 @@ public class HDFCBankAccount implements BankAccount{
         this.balance = balance;
     }
 
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public int getRoi() {
+        return roi;
+    }
+
+    public void setRoi(int roi) {
+        this.roi = roi;
+    }
+
     @Override
     public Boolean withdrawMoney(int money) {
         if(this.balance >= money) {
@@ -21,4 +45,29 @@ public class HDFCBankAccount implements BankAccount{
         }
         return false;
     }
+
+    @Override
+    public Boolean addMoney(int money) {
+        this.balance += money;
+        return true;
+    }
+
+    @Override
+    public int checkBalance() {
+        return this.balance;
+    }
+
+    @Override
+    public int getROI() {
+        return this.roi;
+    }
+
+    @Override
+    public int getTotalInterest(int amount, int timeInYears) {
+        return amount * timeInYears * roi / 100;
+    }
+
+    // min balance = 500
+    // roi should be dependent on age more age -> more roi
+
 }
