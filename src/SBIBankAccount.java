29eb6dfb9bf71962d2bab641429157236744369a
Ritwik <ritwikhero@gmail.com>
2023-05-10 13:29:22 +0530

    SBI bank account modified

--- src/SBIBankAccount.java
+++ src/SBIBankAccount.java
@@ -1,29 +1,68 @@
+import java.util.UUID;
+
 public class SBIBankAccount implements BankAccount{
     private String accountNumber;
     private int balance;
-    private int minBalance;
+    private int minBalance = 500;
     private int roi;
 
+    private int age;
 
+    public SBIBankAccount(int balance) {
+        this.balance = balance;
+        this.accountNumber = String.valueOf(UUID.randomUUID());
+//        this.minBalance = 500;
+        this.roi = 5;
+    }
+
+    public SBIBankAccount(String accountNumber, int balance, int roi, int age) {
+        this.accountNumber = String.valueOf(UUID.randomUUID());
+        this.balance = balance;
+        this.roi = roi;
+        this.age = age;
+    }
 
     @Override
     public Boolean withdrawMoney(int money) {
-        return null;
+        if(balance > minBalance && money <= balance){
+            this.balance -= money;
+            return true;
+        }
+        return false;
     }
 
     @Override
     public Boolean addMoney(int money) {
-        return null;
+        this.balance += money;
+        return true;
     }
 
     @Override
     public int checkBalance() {
-        return 0;
+        return this.balance;
+    }
+
+    public void setRoi(int roi) {
+        if(1 < age && age <= 18){
+            this.roi = 5;
+        }
+        else if (19 <= age &&  age <= 25) {
+            this.roi = 7;
+        }
+        else if(26 <= age && age <= 40){
+            this.roi = 10;
+        }
+        else if (41 <= age && age <= 60) {
+            this.roi = 12;
+        }
+        else{
+            this.roi = 16;
+        }
     }
 
     @Override
     public int getROI() {
-        return 0;
+        return this.roi;
     }
 
     @Override
